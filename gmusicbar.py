99c57c805c012cde75011a36676297dfc3d966e1
vikki <vikkic@vt.edu>
2013-06-07 07:50:49 -0400

    switching from gst to mplayer

--- gmusicbar.py
+++ gmusicbar.py
@@ -1,22 +1,26 @@
 import getpass
-import pygst
-pygst.require("0.10")
-import gst
+import subprocess
 from gmusicapi import Webclient
 
-email = raw_input("Email: ")
-password = getpass.getpass()
+def play(url):
+	fnull = open('/dev/null', 'w')
+	proc = subprocess.Popen(['mplayer', "%s" % url], stdin=subprocess.PIPE, stdout=fnull, stderr=fnull)
+	return proc
 
-api = Webclient()
-api.login(email, password)
-# => True
+def main():
+	email = raw_input("Email: ")
+	password = getpass.getpass()
 
-firstsong = api.get_all_songs()[0]['id']
-print firstsong
+	api = Webclient()
+	api.login(email, password)
 
-url = api.get_stream_url(firstsong)
+	firstsong = api.get_all_songs()[0]['id']
+	print firstsong
 
-player = gst.element_factory_make("playbin2", "player")
-player.set_property('uri', url)
-player.set_state(gst.STATE_PLAYING)
-print url
+	url = api.get_stream_url(firstsong)
+	print url
+
+	cursong = play(url)
+
+if __name__ == '__main__':
+	main()
