1539fbd25d65e53bf3945d227441fdec2655ba8b
vikki <vikkic@vt.edu>
2013-06-08 12:58:32 -0400

    read email from config file, print playlists at start, simple loop with 'q' to exit

--- gmusicbar.py
+++ gmusicbar.py
@@ -1,6 +1,35 @@
+#!/usr/bin/python2
+
+import os
+import sys
+import fcntl
+import termios
 import getpass
 import subprocess
 from gmusicapi import Webclient
+from os.path import expanduser
+
+def getch():
+  fd = sys.stdin.fileno()
+
+  oldterm = termios.tcgetattr(fd)
+  newattr = termios.tcgetattr(fd)
+  newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+  termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+  oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+  fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+  try:        
+    while 1:            
+      try:
+        c = sys.stdin.read(1)
+        break
+      except IOError: pass
+  finally:
+    termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+  return c
 
 def play(url):
 	fnull = open('/dev/null', 'w')
@@ -8,19 +37,32 @@ def play(url):
 	return proc
 
 def main():
-	email = raw_input("Email: ")
-	password = getpass.getpass()
+  home = expanduser('~')
+  configfile = open(home + '/.gmusicbarrc')
 
-	api = Webclient()
-	api.login(email, password)
+  for a in configfile:
+    line = a.split(' ')
+    if (line[0] == 'email'):
+      email = line[1]
+    else:
+      email = raw_input("Email: ")
 
-	firstsong = api.get_all_songs()[0]['id']
-	print firstsong
+  password = getpass.getpass()
+  
+  api = Webclient()
+  api.login(email, password)
 
-	url = api.get_stream_url(firstsong)
-	print url
+  playlists = api.get_all_playlist_ids()
+  for playlist in playlists['user']:
+    print playlist
 
-	cursong = play(url)
+  while True:
+    c = getch() 
+    if (c == 'q'):
+      api.logout() 
+      break
+
+  
 
 if __name__ == '__main__':
 	main()
